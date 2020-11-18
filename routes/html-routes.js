21b3ab2ee00fa4417de31c2dab59087b908cfdca
jordanwhunter <jordanwhunter.mail@gmail.com>
2020-11-18 18:34:53 -0500

    Created HTML paths for stats and exercise

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -5,7 +5,14 @@ const router = require("express").Router();
 const path = require("path");
 
 // Routes
+// I want to be able to view create and track daily workouts
+router.get("/stats", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/stats.html"));
+});
 
+router.get("/exercise", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/exercise.html"));
+});
 
 // Export
 module.exports = router;
