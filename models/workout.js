6c64248ffcc2c8bcce0281612f7bc2e55a4bda45
jordanwhunter <jordanwhunter.mail@gmail.com>
2020-11-18 17:58:14 -0500

    Pseudo-coded outline for workout.js model

---
+++ models/workout.js
@@ -0,0 +1,13 @@
+// Dependencies
+
+// Establish mongoose.schema
+
+// Set up guidelines for schema
+
+  // I want to be able to log multiple exercises in a workout on a given day.
+
+  // I should also be able to track the name, type, weight, sets, reps, and duration of exercise.
+
+  // If the exercise is a cardio exercise, I should be able to track my distance traveled.
+
+// Export
