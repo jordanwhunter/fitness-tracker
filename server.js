ee114c30a1c1950eaeb9e345a67f83226e4e1469
jordanwhunter <jordanwhunter.mail@gmail.com>
2020-11-18 16:37:56 -0500

    Established local port and middleware specifications

--- server.js
+++ server.js
@@ -4,8 +4,16 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 
 // Establish PORT Connection
+const PORT = 3000;
+const app = express();
 
 // Middleware
+app.use(logger("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Set static files to public folder
+app.use(express.static("public"));
 
 // MongoDB Atlas connect
 
