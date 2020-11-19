c1973f94d398b78dfd9a5889f4b8abef4aacc36f
jordanwhunter <jordanwhunter.mail@gmail.com>
2020-11-18 21:46:26 -0500

    Pseudo-coded outline for api-routes.js

---
+++ routes/api-routes.js
@@ -0,0 +1,14 @@
+// Dependencies
+
+// We need CRUD to manipulate schema requirements 
+
+// POSTs, GETs, PUTs, DELETEs
+
+// Looking through api.js, stats.js and workout.js in the public folder, we establish endpoints of our API routes:
+// "/api/workouts"
+
+// "/api/workouts/range"
+
+// "/api/workouts/:id"
+
+// export
