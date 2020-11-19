2b3247d6bd1e9f1bed8a69a7057e8141086e3862
jordanwhunter <jordanwhunter.mail@gmail.com>
2020-11-18 19:23:17 -0500

    Added index route to html-routes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -7,6 +7,10 @@ const path = require("path");
 // Routes
 // I want to be able to view create and track daily workouts
 // Stats and exercise routes determined from looking at index.html
+router.get("/", (req,res) => {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
 router.get("/stats", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/stats.html"));
 });
