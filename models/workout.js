3cd0bb2327cb93790f335f005412b214c5d2b462
jordanwhunter <jordanwhunter.mail@gmail.com>
2020-11-19 00:21:14 -0500

    Built dynamic schema referencing totalDuration which is found in the public workout.js file

--- models/workout.js
+++ models/workout.js
@@ -47,12 +47,22 @@ const schemaForWorkouts = new Schema (
     }]
   },
   {
-    // For reference on virtuals with json: https://mongoosejs.com/docs/2.7.x/docs/virtuals.html - when data is called, this will include any virtual properties
+    // For reference on virtuals with json: https://mongoosejs.com/docs/2.7.x/docs/virtuals.html - when data is called, this will include any virtual properties.
+    // The toJSON() method converts a Date object into a string, formatted as a JSON date. For reference: https://www.w3schools.com/jsref/jsref_tojson.asp
     toJSON: {
       virtuals: true
     }
   }
 );
+
+// dynamic schema referencing totalDuration which is found in the workout.js file contained in the public folder
+schemaForWorkouts.virtual("totalDuration").get(function () {
+  // cannot use arrow function on account of the .this property referencing exercises
+  // reduce() to handle limiting the array of exercises down to their duration totals
+  return this.exercises.reduce((total, exercise) => {
+    return total + exercise.duration;
+  }, 0);
+});
 
 // Export
 const Workout = mongoose.model("Workout", schemaForWorkouts);
