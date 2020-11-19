a18523cd9c1844e1818e10b7adfa78066f417994
jordanwhunter <jordanwhunter.mail@gmail.com>
2020-11-18 22:07:15 -0500

    Created posts and gets for /api/workouts endpoint

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -3,10 +3,30 @@ const router = require("express").Router();
 
 const Workout = require("../models/workout.js");
 
-// We need CRUD to manipulate schema requirements (POSTs, GETs, PUTs, DELETEs)
+// We need CRUD to manipulate schema requirements (POSTs, GETs, PUTs, DELETEs). Looking through api.js, stats.js and workout.js in the public folder, we establish endpoints of our API routes:
 
-// Looking through api.js, stats.js and workout.js in the public folder, we establish endpoints of our API routes:
 // "/api/workouts"
+// CREATE
+router.post("/api/workouts", (req, res) => {
+  Workout.create({})
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+// READ
+router.get("/api/workouts", (req, res) => {
+  Workout.find()
+    .then(dbWorkouts => {
+      res.json(dbWorkouts);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
 
 // "/api/workouts/range"
 
